3edf67d4651b0707f8454ab5a4267d587999ffa1
Stefan Darrell Davis <60591525+Davisstef@users.noreply.github.com>
2022-01-22 23:01:35 -0500

    Update Leaders.js

--- client/src/components/Leaders.js
+++ client/src/components/Leaders.js
@@ -2,26 +2,22 @@
 
 function Leaders () {
   return (
-    <html>
-    <head>
-        <style>
-            #input-form {
-                padding: 15px;
-                background-color: #CCCCCC;
-            }
-            #input-form input {
-                padding: 10px;
-                font-size: 20px;
-                width: 400px;
-            }
-        </style>
-    </head>
-    <body>
-        <div id="input-form">
-            <input type="text" name="username" placeholder="Enter a Name" />
-        </div>
-    </body>
-</html>
+    <!DOCTYPE html>
+<html>
+	<head>
+		<title>LeaderBoard</title>
+	</head>
+
+	<body>
+		<h2>High Scores</h2>
+		<table>
+			<tr>
+				<td>Ranking</td>
+				<td>UserName</td>
+				<td>Marks</td>
+			</tr>
+  </body>
+  </html>
   )
 };
 
