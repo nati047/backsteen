1bc4d51b58e2ae7afa24d505748e4a9d7385e865
Stefan Darrell Davis <60591525+Davisstef@users.noreply.github.com>
2022-01-22 23:04:11 -0500

    Update Leaders.js

--- client/src/components/Leaders.js
+++ client/src/components/Leaders.js
@@ -1,23 +1,14 @@
-<<    import React from "react";
+import React from "react";
 
 function Leaders () {
   return (
-    <!DOCTYPE html>
-<html>
-	<head>
-		<title>LeaderBoard</title>
-	</head>
-
-	<body>
-		<h2>High Scores</h2>
-		<table>
-			<tr>
-				<td>Ranking</td>
-				<td>UserName</td>
-				<td>Marks</td>
-			</tr>
-  </body>
-  </html>
+    <html>
+    <body>
+        <div id="input-form">
+            <input type="text" name="username" placeholder="Enter a Name" />
+        </div>
+    </body>
+    </html>
   )
 };
 
